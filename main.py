58028f637f513858246aaeee43785c3a7d43ac55
Hibber <travis@triddle.dev>
2022-05-01 20:57:13 -0700

    Added new on_ready status. Shows how many servers the bot is in as well as server members.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+
 import nextcord
 import os
 import requests
@@ -7,26 +8,28 @@ from blizzardapi import BlizzardApi
 from dotenv import load_dotenv
 from nextcord.ext import commands
 
-
 from auth import get_token
 
 load_dotenv()
 bot_token = os.getenv("BOT_TOKEN")
 bot = commands.Bot(command_prefix="!")
 access_token = os.getenv("ACCESS_TOKEN")
-api = BlizzardApi(os.getenv("WOW_CLIENT_ID"), os.getenv("WOW_CLIENT_SECRET"))
+api = BlizzardApi(os.getenv("WOW_bot_ID"), os.getenv("WOW_bot_SECRET"))
 
 
 @bot.event
 async def on_ready():
-    print("Logged in as")
-    print(bot.user.name)
-    print(bot.user.id)
-    print("------")
-    await bot.change_presence(
-        status=nextcord.Status.online, activity=nextcord.Game("!help")
-    )
-    print(access_token)
+    print('Potato Cat is ready :D')
+
+    servers = len(bot.guilds)
+    members = 0
+    for guild in bot.guilds:
+        members += guild.member_count - 1
+
+    await bot.change_presence(activity=nextcord.Activity(
+        type=nextcord.ActivityType.watching,
+        name=f'{servers} servers and {members} members'
+    ))
 
 
 @bot.command(name="token", help="Get WoW token price")
@@ -39,10 +42,10 @@ async def get_wow_token(ctx):
     response = requests.get(url, headers=headers).json()
     token = response["price"]
     formatted = int(str(int(str(token)[::-1]))[::-1])
-    text_title = "🪙Current Token Price for US🪙"
+    text_title = "🪙WoW Token🪙"
+
     embed = nextcord.Embed(title=text_title + "\n\n", color=0x00FF00,
-                           description='**Price:** ' + f"**`{formatted}`**")
-    await ctx.send("<@" + str(ctx.author.id) + ">")
+                           description='**Price:** ' + f"**`{formatted}`**" + '\n\n')
     embed.set_author(
         name="GSMB Bot", url="https://worldofwarcraft.com/en-us/token",
         icon_url="https://render.worldofwarcraft.com/us/guild/crest/69/emblem-69-b1b8b1"
@@ -56,7 +59,8 @@ async def get_wow_token(ctx):
         text="🌐 " + datetime.datetime.now().strftime("%A " + "at: " + "%H:%M" + "%p")
     )
     channel = await bot.fetch_channel(960708949127102484)
-    await channel.send(embed=embed)
+    await channel.send("<@" + str(ctx.author.id) + ">" + "\n", embed=embed)
+    # await channel.send(embed=embed)
 
 
 # @bot.command(name="character")
